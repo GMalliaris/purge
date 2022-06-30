be6614749fe900964ae962c1796a73b15937f4f7
gmalliaris <gmalliaris@mtg-hellas.gr>
2022-06-30 15:19:03 +0300

    initial commit

---
+++ src/main/java/org/gmalliaris/Purge.java
@@ -0,0 +1,11 @@
+package org.gmalliaris;
+
+import java.util.Arrays;
+
+public class Purge
+{
+    public static void main(String[] args)
+    {
+        System.out.println(Arrays.asList(args));
+    }
+}
